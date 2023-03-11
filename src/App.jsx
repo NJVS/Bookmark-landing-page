a4e39a247a7d8ca41e6d613faa9af5e14532f91e
NJVS <neiljonathanv.sagisi+github@gmail.com>
2023-03-11 14:12:13 +0800

    adjustments

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import Navbar from './components/Navbar/Navbar';
+import Downloads from './components/sections/Downloads/Downloads';
+import Faq from './components/sections/Faq/Faq';
+import Features from './components/sections/Features/Features';
+import Hero from './components/sections/Hero/Hero';
+import Subscribe from './components/sections/Subscribe/Subscribe';
 
 function App() {
   return (
     <>
       <Navbar />
-      <main></main>
+      <main>
+        <Hero />
+        <Features />
+        <Downloads />
+        <Faq />
+        <Subscribe />
+      </main>
       <Navbar position='footer' />
     </>
   );
